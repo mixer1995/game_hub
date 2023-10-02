85f44d38c6c32d3f0c5c2aae062507c5ffb7c46f
Maksym Shkolnyi <mixer19995@gmail.com>
2023-10-02 11:07:09 +0300

    Built a NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 import { Grid, GridItem, Show } from '@chakra-ui/react'
+import NavBar from './components/NavBar';
 
 function App() {
   return (
@@ -9,7 +7,9 @@ function App() {
       base: `"nav" "main"`,
       lg: `"nav nav" "aside main"`
     }}>
-      <GridItem area='nav' bg='coral'>Nav</GridItem>
+      <GridItem area='nav'>
+        <NavBar />
+      </GridItem>
       <Show above='lg'>
         <GridItem area='aside' bg='gold'>Aside</GridItem>
       </Show>
