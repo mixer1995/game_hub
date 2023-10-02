bbacd28cf9d702213e6f7555a9f8243590b68434
Maksym Shkolnyi <mixer19995@gmail.com>
2023-10-02 11:19:44 +0300

    Build color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,9 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above='lg'>
-        <GridItem area='aside' bg='gold'>Aside</GridItem>
+        <GridItem area='aside'>Aside</GridItem>
       </Show>
-      <GridItem area='main' bg='dodgerBlue'>Main</GridItem>
+      <GridItem area='main'>Main</GridItem>
     </Grid>
   );
 }
