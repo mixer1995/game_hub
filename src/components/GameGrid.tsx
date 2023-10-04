03230c3e258bca808182f002161de3fc0bd0270d
Maksym Shkolnyi <mixer19995@gmail.com>
2023-10-04 11:01:48 +0300

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,7 +13,7 @@ const GameGrid = () => {
     return (
         <>
             {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding='10px' spacing={10}>
+            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding='10px' spacing={3}>
                 {isLoading && skeletons.map(skeleton =><GameCardContainer key={skeleton}><GameCardSkeleton/></GameCardContainer>)}
                 {data.map(game => <GameCardContainer key={game.id}><GameCard game={game} /></GameCardContainer>)}
             </SimpleGrid>
