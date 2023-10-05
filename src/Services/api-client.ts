4142ece47f8443053ae994d991e843ef418c1ed8
Maksym Shkolnyi <mixer19995@gmail.com>
2023-10-05 10:01:47 +0300

    Final cleanup 2

--- src/Services/api-client.ts
+++ src/Services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://api.rawg.io/api',
+    baseURL: 'https://api.rawg.io/api',
     params: {
         key: 'f638cd4aaaeb4dbfbc32f91084457220'
     }
